b6beb429565c8a5631823e5bba4bcd82ec08b8ca
Karl Wallner <kwallner@mail.de>
2020-12-19 01:33:53 +0100

    Build of mosquito now works (just tested for one config). Needs further improvement.

--- recipes/mosquitto/all/conanfile.py
+++ recipes/mosquitto/all/conanfile.py
@@ -9,11 +9,11 @@ class MosquittoConan(ConanFile):
     description = "Eclipse Mosquitto - An open source MQTT broker"
     topics = ("conan", "eclipse", "mqtt", "broker", "protocol")
     license = "Eclipse Public License 2.0"
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = ["CMakeLists.txt", "patches/*"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [True, False], "fPIC": [True, False], "with_openssl": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "with_openssl": True}
     no_copy_source = True
 
     _cmake = None
@@ -26,10 +26,15 @@ class MosquittoConan(ConanFile):
         self.requires("cjson/1.7.14")
         self.requires("openssl/1.1.1i")
 
+    def _patch_sources(self):
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(patch_file=patch['patch_file'], strip=1)
+
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
+        self._patch_sources()
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -43,15 +48,18 @@ class MosquittoConan(ConanFile):
         if self._cmake:
             return self._cmake
         self._cmake = CMake(self)
-        self._cmake.definitions["WITH_BUNDLED_DEPS"] = False
-        self._cmake.definitions["WITH_TLS"] = True # SSL Support
-        self._cmake.definitions["WITH_TLS_PSK"] = False # SSL Support
-        self._cmake.definitions["WITH_EC"] = False
+        self._cmake.definitions["CJSON_DIR"] = self.deps_cpp_info["cjson"].rootpath #.replace('\', '/'')
+        self._cmake.definitions["WITH_BUNDLED_DEPS"] = True
+        self._cmake.definitions["WITH_TLS"] = self.options.with_openssl
+        self._cmake.definitions["WITH_TLS_PSK"] = self.options.with_openssl
+        self._cmake.definitions["WITH_EC"] = self.options.with_openssl
         self._cmake.definitions["WITH_UNIX_SOCKETS"] = False
         self._cmake.definitions["WITH_SOCKS"] = False
         self._cmake.definitions["WITH_SRV"] = False
 
         self._cmake.definitions["WITH_STATIC_LIBRARIES"] = not self.options.shared
+        self._cmake.definitions["WITH_SHARED_LIBRARIES"] = self.options.shared
+        
         self._cmake.definitions["WITH_PIC"] = "fPIC" in self.options and self.options.fPIC
         self._cmake.definitions["WITH_THREADING"] = False
         self._cmake.definitions["WITH_DLT"] = False
@@ -61,7 +69,7 @@ class MosquittoConan(ConanFile):
         self._cmake.definitions["WITH_APPS"] = True
         self._cmake.definitions["WITH_PLUGINS"] = True
         self._cmake.definitions["DOCUMENTATION"] = True
-       
+
         self._cmake.configure()
         return self._cmake
 
@@ -73,14 +81,7 @@ class MosquittoConan(ConanFile):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-
+        
     def package_info(self):
-        zstd_cmake = "libzstd_shared" if self.options.shared else "libzstd_static"
-        self.cpp_info.components["zstdlib"].names["pkg_config"] = "libzstd"
-        self.cpp_info.components["zstdlib"].names["cmake_find_package"] = zstd_cmake
-        self.cpp_info.components["zstdlib"].names["cmake_find_package_multi"] = zstd_cmake
-        self.cpp_info.components["zstdlib"].libs = tools.collect_libs(self)
-        if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.components["zstdlib"].system_libs.append("pthread")
+        #self.cpp_info.components["mosquitto"].libs = tools.collect_libs(self)
+        pass
